9918e8a8345a27567de62af8d5ed267b8a1b0924
Ricardo F Pereira <ricardofonseca_1@hotmail.com>
2024-02-26 16:06:57 -0300

    init SGA Application

---
+++ src/dataFake.js
@@ -0,0 +1,38 @@
+export const thoughts = [
+
+    {
+    title: "Pensamento 1",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dapibus, ex scelerisque suscipit interdum, lectus ante placerat leo, ac blandit ex mauris eu metus. Maecenas posuere fermentum ex, nec facilisis ligula gravida congue. Nunc facilisis eros tellus, in finibus ipsum porta pretium. Ut velit nulla, dapibus vitae fringilla ac, auctor in dolor. Morbi sed turpis non lorem lobortis tempor. Curabitur vestibulum justo non lorem cursus scelerisque. Integer vitae dui sit amet lacus semper tincidunt id sit amet mauris. Praesent aliquam lorem diam, sed posuere sapien consectetur in. Ut dignissim nibh a interdum volutpat. Nam convallis rutrum arcu, sed faucibus odio aliquet condimentum. Morbi semper non libero at eleifend. Aenean eu lorem nisi. Integer ornare turpis et urna feugiat ornare. Curabitur ullamcorper, ante non ornare gravida, leo arcu ornare odio, in sollicitudin risus quam nec risus. Praesent sed magna mollis risus condimentum porttitor.",
+    likes: 3,
+    comments: 1
+    },
+    
+    {
+        title: "Pensamento 2",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dapibus, ex scelerisque suscipit interdum, lectus ante placerat leo, ac blandit ex mauris eu metus. Maecenas posuere fermentum ex, nec facilisis ligula gravida congue. Nunc facilisis eros tellus, in finibus ipsum porta pretium. Ut velit nulla, dapibus vitae fringilla ac, auctor in dolor. Morbi sed turpis non lorem lobortis tempor. Curabitur vestibulum justo non lorem cursus scelerisque. Integer vitae dui sit amet lacus semper tincidunt id sit amet mauris. Praesent aliquam lorem diam, sed posuere sapien consectetur in. Ut dignissim nibh a interdum volutpat. Nam convallis rutrum arcu, sed faucibus odio aliquet condimentum. Morbi semper non libero at eleifend. Aenean eu lorem nisi. Integer ornare turpis et urna feugiat ornare. Curabitur ullamcorper, ante non ornare gravida, leo arcu ornare odio, in sollicitudin risus quam nec risus. Praesent sed magna mollis risus condimentum porttitor.",
+        likes: 3,
+        comments: 1
+        },
+        
+    {
+        title: "Pensamento 3",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dapibus, ex scelerisque suscipit interdum, lectus ante placerat leo, ac blandit ex mauris eu metus. Maecenas posuere fermentum ex, nec facilisis ligula gravida congue. Nunc facilisis eros tellus, in finibus ipsum porta pretium. Ut velit nulla, dapibus vitae fringilla ac, auctor in dolor. Morbi sed turpis non lorem lobortis tempor. Curabitur vestibulum justo non lorem cursus scelerisque. Integer vitae dui sit amet lacus semper tincidunt id sit amet mauris. Praesent aliquam lorem diam, sed posuere sapien consectetur in. Ut dignissim nibh a interdum volutpat. Nam convallis rutrum arcu, sed faucibus odio aliquet condimentum. Morbi semper non libero at eleifend. Aenean eu lorem nisi. Integer ornare turpis et urna feugiat ornare. Curabitur ullamcorper, ante non ornare gravida, leo arcu ornare odio, in sollicitudin risus quam nec risus. Praesent sed magna mollis risus condimentum porttitor.",
+        likes: 3,
+        comments: 1
+        },
+        
+    {
+        title: "Pensamento 4",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dapibus, ex scelerisque suscipit interdum, lectus ante placerat leo, ac blandit ex mauris eu metus. Maecenas posuere fermentum ex, nec facilisis ligula gravida congue. Nunc facilisis eros tellus, in finibus ipsum porta pretium. Ut velit nulla, dapibus vitae fringilla ac, auctor in dolor. Morbi sed turpis non lorem lobortis tempor. Curabitur vestibulum justo non lorem cursus scelerisque. Integer vitae dui sit amet lacus semper tincidunt id sit amet mauris. Praesent aliquam lorem diam, sed posuere sapien consectetur in. Ut dignissim nibh a interdum volutpat. Nam convallis rutrum arcu, sed faucibus odio aliquet condimentum. Morbi semper non libero at eleifend. Aenean eu lorem nisi. Integer ornare turpis et urna feugiat ornare. Curabitur ullamcorper, ante non ornare gravida, leo arcu ornare odio, in sollicitudin risus quam nec risus. Praesent sed magna mollis risus condimentum porttitor.",
+        likes: 3,
+        comments: 1
+        },
+        
+    {
+        title: "Pensamento 5",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dapibus, ex scelerisque suscipit interdum, lectus ante placerat leo, ac blandit ex mauris eu metus. Maecenas posuere fermentum ex, nec facilisis ligula gravida congue. Nunc facilisis eros tellus, in finibus ipsum porta pretium. Ut velit nulla, dapibus vitae fringilla ac, auctor in dolor. Morbi sed turpis non lorem lobortis tempor. Curabitur vestibulum justo non lorem cursus scelerisque. Integer vitae dui sit amet lacus semper tincidunt id sit amet mauris. Praesent aliquam lorem diam, sed posuere sapien consectetur in. Ut dignissim nibh a interdum volutpat. Nam convallis rutrum arcu, sed faucibus odio aliquet condimentum. Morbi semper non libero at eleifend. Aenean eu lorem nisi. Integer ornare turpis et urna feugiat ornare. Curabitur ullamcorper, ante non ornare gravida, leo arcu ornare odio, in sollicitudin risus quam nec risus. Praesent sed magna mollis risus condimentum porttitor.",
+        likes: 3,
+        comments: 1
+        }
+
+]
